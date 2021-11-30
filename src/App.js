3a1e144f97faa1df032ca16125d90c5ec7d0f091
Massoud Javadi <Masoudjavadi180@gmail.com>
2021-11-30 10:42:36 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+
+function List(props) {
+  return(
+    <div>
+      {props.items.map((item,index)=>{
+        return <h1 key={index}>{item}</h1>
+      })}
+    </div>
+  )
+}
+
+const MetaList = ({items}) =>{
+  return(
+    <div>
+      {items.map((item,index)=>{
+        return <h1 key={index}>{item}</h1>
+      })}
+    </div>
+  )
+
+}
+function ItemCount(props) {
+   
+   return <h1>{props.count}</h1>
+}
 
 function App() {
+
+  const [items, setItems] = useState(['ali','massoud','hossein'])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List items={items}></List>
+      <ItemCount count={items.length}></ItemCount>
+      <MetaList items={items}></MetaList>
     </div>
   );
 }
